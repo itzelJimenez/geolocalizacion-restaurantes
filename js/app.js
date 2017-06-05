24cb338c644178dc89bbd469d56194a035173782
Itzel <itzeljimenez.do@gmail.com>
2017-06-05 21:12:36 +0000

    enlace con barra de búsqueda

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
   //Función que inicia todas las demás funciones
 var cargarPagina = function(){
   initMap();
+  $("#search-form").submit(filtrar);
 }
 
 var $mapaContenedor = $('#map');
@@ -30,9 +31,44 @@ var error = function(){
     alert("Lo sentimos, por el momento no podemos acceder a tu ubicación, intentalo más tarde.")
 }
 
-var restarantes =[
- 
+var restarantes = [
+  {
+    "nombre": "Mercado Roma",
+    "ubicación": {lat:19.4154167,  lng: -99.1646039 }
+  },
+  {
+    "nombre": "Roseta",
+    "ubicación": {lat:19.4154167,  lng: -99.1646039 }
+  },
+  {
+    "nombre": "Lalo!",
+    "ubicación": {lat:19.4154167,  lng: -99.1646039 }
+  },
+  {
+    "nombre": "Mercado Roma",
+    "ubicación": {lat:19.4154167,  lng: -99.1646039 }
+  },
+  {
+    "nombre": "Mercado Roma",
+    "ubicación": {lat:19.4154167,  lng: -99.1646039 }
+  },
+  {
+    "nombre": "Mercado Roma",
+    "ubicación": {lat:19.4154167,  lng: -99.1646039 }
+  }
 ];
+
+var plantillaContacto = '<div class="well  well-lg page-header">' +
+        '<div class="card-panel hoverable grey lighten-5 z-depth-1">' +
+          '<h4>**Nombre**</h4>' +
+            '<p>Puntuación:</p>' +
+              '<button class="btn btn-primary btn-md ">Ver ubicación</button>' +
+            '</div>' +
+        '</div>';
+  var filtrar = function (e) {
+  e.preventDefault();
+  alert("hola");
+};
 
   $(document).ready(cargarPagina);
 }) ();
