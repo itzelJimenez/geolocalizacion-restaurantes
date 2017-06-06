14962b4a8178782a7abaf7cd5d31361189d57155
Itzel <itzeljimenez.do@gmail.com>
2017-06-06 11:00:13 +0000

    Búsqueda dinámica y muestra de lugares dinámicamente

--- js/app.js
+++ js/app.js
@@ -1,34 +1,36 @@
 (function(){
   //Función que inicia todas las demás funciones
-var $botonBuscar=$('.buscar');
+var $botonBuscar=$('.well');
 var $mapaContenedor = $('#map');
 
 var cargarPagina = function(){
   initMap();
   $("#search-form").submit(filtrar);
-  $botonBuscar.click(function(){alert("hola");})
+  $(document).on("click", ".buscar", cambiarUbicacion)
 }
 
 var initMap = function(){
     if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(mostrarCoordenadas, error);
-    }
+      navigator.geolocation.getCurrentPosition(mostrarPosiciónActual, error);
+    } else{error()};
 };
-var mostrarCoordenadas = function(cords){
+var mostrarPosiciónActual = function(cords){
     var latitud= (cords.coords.latitude);
     var longitud=(cords.coords.longitude);
+    var coordenadas = {lat:latitud, lng:longitud};
+    mostrarMapa(coordenadas);
 
+}
+var mostrarMapa = function(coordenadas){
     var mapa = new google.maps.Map($mapaContenedor[0],{
        zoom:18,
-        center: {lat: latitud, lng: longitud}
+        center: coordenadas
       });
     var marcador = new google.maps.Marker({
-      position: {lat: latitud, lng: longitud},
+      position: coordenadas,
       map:mapa,
-      title: "My ubicación",
-      draggable: true,
     });
-}
+};
 var error = function(){
     
     alert("Lo sentimos, por el momento no podemos acceder a tu ubicación, intentalo más tarde.")
@@ -37,27 +39,33 @@ var error = function(){
 var restaurantes = [
   {
     "nombre": "Mercado Roma",
-    "ubicación": {lat:19.414062,  lng: -99.1664147 }
+    "latitud": 19.414062,  
+    "longitud": -99.1664147 
   },
   {
     "nombre": "Rosseta",
-    "ubicación": {lat:19.419869,  lng: -99.1620097 }
+    "latitud": 19.419869,
+    "longitud": -99.1620097
   },
   {
     "nombre": "Lalo!",
-    "ubicación": {lat:19.4154167,  lng: -99.1646039}
+    "latitud": 19.4154167,  
+    "longitud": -99.1646039
   },
   {
     "nombre": "El parnita",
-    "ubicación": {lat:19.4140759,  lng: -99.1648121 }
+    "latitud": 19.4140759,  
+    "longitud": -99.1648121
   },
   {
     "nombre": "Mog Bistro",
-    "ubicación": {lat:19.4161948,  lng: -99.1572155 }
+    "latitud": 19.4161948,  
+    "longitud": -99.1572155 
   },
   {
     "nombre": "Maximo Bistrot",
-    "ubicación": {lat:19.4152165,  lng: -99.164261 }
+    "latitud": 19.4152165,  
+    "longitud": -99.164261
   }
 ];
 
@@ -65,9 +73,10 @@ var plantillaRestaurante = '<div class="well  well-lg page-header">' +
         '<div class="card-panel hoverable grey lighten-5 z-depth-1">' +
           '<h4>**nombre**</h4>' +
             '<p>Puntuación:</p>' +
-              '<button class="btn btn-primary btn-md buscar">Ver ubicación</button>' +
+              '<button class="btn btn-primary btn-md buscar" data-lat="**latitud**" data-lng="**longitud**">Ver ubicación</button>' +
             '</div>' +
         '</div>';
+
 var filtrar = function (e) {
   e.preventDefault();
   var criterioBusqueda = $("#search").val().toLowerCase();
@@ -81,10 +90,22 @@ var filtrar = function (e) {
 var mostrarRestaurantes = function (restaurantes) {
   var plantillaFinal = "";
   restaurantes.forEach(function (restaurante) {
-    plantillaFinal += plantillaRestaurante.replace("**nombre**", restaurante.nombre)
+    plantillaFinal += plantillaRestaurante.replace("**nombre**", restaurante.nombre).replace("**latitud**", restaurante.latitud).replace("**longitud**", restaurante.longitud)
   });
   $("#lugaresComida").html(plantillaFinal);
 };
+
+function cambiarUbicacion(){
+  var latitud = $(this).data("lat");
+  var longitud =  $(this).data("lng");
+  var coordenadas ={
+    lat: latitud,
+    lng:longitud
+  }
+  console.log(latitud);
+  console.log(coordenadas);
+  mostrarMapa(coordenadas);
+}
 
   $(document).ready(cargarPagina);
 }) ();
